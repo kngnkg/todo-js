dda6436e9b9bb79907f8065db6308fcd58c803be
ryokawa <kawata.ryo.0518@gmail.com>
2022-12-20 19:24:48 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", function (req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.write("Hello World!");
+    res.end();
+});
+
+server.listen(8080, "127.0.0.1");
+console.log("server listen...");
